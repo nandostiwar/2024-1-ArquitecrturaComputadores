ba1ab67ad380e5d671a936a80c272818781ef3de
brandonstevenz2002 <brandonstevenvm@example.com>
2024-03-28 16:00:42 -0500

    Habilitar el seguimiento de archivos en la carpeta dist

--- proyecto-general-horoscopo/horoscopo/vite.config.js
+++ proyecto-general-horoscopo/horoscopo/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true // Agrega esta línea para limpiar la carpeta de salida antes de la construcción
+  }
+});
+
+console.log('Configuración de Vite cargada.');
+ // Agregar un console.log para verificar si la configuración se carga correctamente
+
+
+
+
+
+
